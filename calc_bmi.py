ae951c9e0c6427e133a108cb7f856a9878bc0662
isowa saki <s.isowa.sys20@morijyobi.ac.jp>
2021-03-26 11:03:32 +0900

    main.pyとcalc_bmi.pyと.gitignoreの新規作成

---
+++ calc_bmi.py
@@ -0,0 +1,4 @@
+def get_bmi(kg,h):
+    bmi = kg / (h*h)
+    return bmi
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import calc_bmi
+
+print("BMI計算アプリ")
+kg = int(input("体重："))
+h = int(input("身長："))
+print(f"BMI：{calc_bmi.get_bmi(kg,h)}")
+
