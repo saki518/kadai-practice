ac5686040536dc46c7ba0a0036987e4d24e75ca7
isowa saki <s.isowa.sys20@morijyobi.ac.jp>
2021-03-26 11:10:16 +0900

    main.pyを修正

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import calc_bmi
 print("BMI計算アプリ")
 kg = int(input("体重："))
 h = int(input("身長："))
-print(f"BMI：{calc_bmi.get_bmi(kg,h)}")
+print(f"BMI:{calc_bmi.get_bmi(kg,h)}")
+print(f"BMI:{calc_bmi.get_bmi(kg,h)}")
+
+
 
